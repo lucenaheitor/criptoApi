47f71e18f3b5a7df51d3bf2aedb35362d4a1d572
lucenaheitor <lucenaheitor@gmail.com>
2023-03-01 22:26:50 -0300

    implentação do tratamento de erro e estilação das infos da api

--- src/components/consumoApi/ConsumoApi.js
+++ src/components/consumoApi/ConsumoApi.js
@@ -1,24 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../consumoApi/api.css'
 
-function ConsumoApi(){
-    const [data, setData] = useState([]);
+function ConsumoApi() {
+  const [dataAPI, setDataAPI] = useState([]);
 
-    useEffect(() => {
-      fetch('https://api.coinranking.com/v2/coin/Qwsogvtv82FCd')
-        .then(response => response.json())
-        .then(data => setData(data));
-    }, []);
-    
-  
-    return (
-      <div>
-        <body>
-      <h1> sei la </h1>
-        </body>
+  useEffect(() => {
+    let cancel;
+    axios.get('https://api.coinranking.com/v2/coins', {
+      cancelToken: new axios.CancelToken((c) => cancel = c)
+    })
+    .then(res => {
+      setDataAPI(res.data?.data?.coins || []);
+    })
+    .catch(err => {
+      if (axios.isCancel(err)) return;
+      console.error(err);
+      setDataAPI([]);
+    });
+
+    return () => cancel();
+  }, []);
+
+  return (
+    <div>
+      <h1> Moedas</h1>
+      <div className="api-container">
+        {dataAPI.map((coin) => (
+          <p key={coin.id}>{coin.name} ({coin.symbol}): {coin.marketCap}</p>
+        ))}
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-
-
-export default ConsumoApi
+export default ConsumoApi;
